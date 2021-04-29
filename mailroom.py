e99643c79509be4b2ddb07652c1895187c586e29
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-29 13:09:01 -0700

    Did a complete overhaul on my program. Changed the data structure I used. I realized after the demo from class that I had misread the data requirements. Also created a test file using pytest per the assignment instructions

--- mailroom.py
+++ mailroom.py
@@ -1,4 +1,5 @@
-'''Write a small command-line script called mailroom.py. This script should be executable.
+'''
+Write a small command-line script called mailroom.py. This script should be executable.
 The script should accomplish the following goals:
 
 It should have a data structure that holds a list of your donors and a history of the amounts they have donated.
@@ -9,12 +10,12 @@ The script should prompt the user (you) to choose from a menu of 3 actions:
 “Send a Thank You”, “Create a Report” or “quit”.
 '''
 
-donor_list = ['Tony Stark',
-              'Steven Rogers',
-              'Bruce Wayne',
-              'Clark Kent',
-              'Geoffrey Donnell']
-donation_history = [[10000], [200, 300], [1000, 2000, 3000], [100], [23000]]
+donor_data = [('Tony Stark', [10]),
+              ('Steven Rogers', [10, 20]),
+              ('Bruce Wayne', [20, 30, 40]),
+              ('Clark Kent', [50]),
+              ('Geoffrey Donnell', [60, 70])]
+
 
 def menu():
     # This function will be the one that runs everytime and will point to other functions to complete
@@ -41,83 +42,114 @@ def menu():
     return
 
 
+def display_donors():
+    '''
+    Function that will print the list of donors
+    '''
+
+    print('\n')
+    for step in range(0, len(donor_data)):
+        print(donor_data[step][0])
+    print('\n')
+
+
+def generate_donor_list():
+    '''
+    Function that will generate a donor_list to check against
+    to see if the input from user entering an existing donor
+    '''
+    donor_list = [''] * len(donor_data)
+    for step in range(0, len(donor_data)):
+        donor_list[step] = donor_data[step][0]
+    return donor_list
+
+
 def current_donor(donor_name, new_donation):
     """" For donors that are already on file the function adds the new donation to their history"""
-    # Start of loop to determine where in the list the donor is and their corresponding amount and freq
-    for index in range(0, int(len(donor_list))):
-        if donor_list[index] == donor_name: # if statement to find where to add the new donation
-            donation_history[index].append(new_donation)
-    return donor_list and donation_history
+    # Loops to determine where in the list the donor is and their corresponding donations
+    for step in range(0, len(donor_data)):
+        if donor_data[step][0] == donor_name:  # if statement to find where to add the new donation
+            donor_data[step][1].append(new_donation)
+    return donor_data
 
-def new_donor(donor_name,new_donation):
-    donor_list.append(((donor_name)))
-    donation_history.append([float(new_donation)])
-    return donor_list and donation_history
+
+def add_new_donor_to_database(donor_name, new_donation):
+    '''For donors not on the list adds them to the list'''
+    new_donor = (donor_name, [new_donation])
+    donor_data.append(new_donor)
+    print(donor_data)
+    return donor_data
+
 
 def letter_options():
     donor_name = input("Please enter the full name of the donor (case sensitive) or type L for a list of donors: ")
     if donor_name == 'L':
-        print(f'The list of donors are: {donor_list}')
+        display_donors()
         letter_options()
+    donor_list = generate_donor_list()
     if donor_name in donor_list:
         new_donation = float(input('We have found this donor in our data base.\n'
                                    'How much is this additional donation? Please enter a value: '))
         current_donor(donor_name, new_donation)
         draft_email(donor_name, new_donation)
+        menu()
     else:
         new_donation = float(input('Yay we have a new donor!\n'
                                    'How much is this new donation? Please enter a value: '))
-        new_donor(donor_name, new_donation)
+        add_new_donor_to_database(donor_name, new_donation)
+        print(donor_data)
         draft_email(donor_name, new_donation)
+        menu()
     pass
 
 
 def draft_email(donor_name, donor_amount):
-    print(f'\nEmail to: {donor_name.replace(" ","")}@gmail.com')
+    donor_amount = '{:.2f}'.format(donor_amount)
+    print(f'\nEmail to: {donor_name.replace(" ", "")}@gmail.com')
     print('Subject: Donation Received')
     print(f'\nThank you {donor_name} for your generous donation of ${donor_amount}! \n'
           f'We greatly appreciate your support to our cause. \n'
           f'Best regards,\n'
-          f'Staff Member\n \n')
-    menu()
-    pass
+          f'Staff Member\n')
+    return (f'Thank you {donor_name} for your generous donation of ${donor_amount}.')
+
 
 def generate_report_data():
-    #Need to delcare empty lists to store values that will be calculated
-    no_donors = int(len(donor_list))
+    # Need to declare empty lists to store values that will be calculated
+    print(f'This is from the generate report data function \n {donor_data}')
+    no_donors = int(len(donor_data))
+    print(f'This is the number of donors {no_donors}')
     donation_freq = [0] * no_donors
     donation_total = [0] * no_donors
-    donation_average = [0]* no_donors
+    donation_average = [0] * no_donors
     # Finds the average and number of donations
     for index in range(0, no_donors):
-        donation_total[index] = sum(donation_history[index])
-        donation_freq[index] = len(donation_history[index])
-        donation_average[index] = donation_total[index]/donation_freq[index]
+        donation_total[index] = sum(donor_data[index][1])
+        donation_freq[index] = len(donor_data[index][1])
+        donation_average[index] = float(donation_total[index] / donation_freq[index])
     return no_donors, donation_total, donation_freq, donation_average
 
 
 def print_report():
     no_donors, donation_total, donation_freq, donation_average = generate_report_data()
-    #Formating and Printing for Header
-    header = ['Donor Name','Total Given','Number Gifts','Average Gifts']
+    # Formating and Printing for Header
+    header = ['Donor Name', 'Total Given', 'Number Gifts', 'Average Gifts']
     header_format = '{:18}|{:16}|{:16}|{:20}|'
     print('\n')
     print(header_format.format(*header))
-    print('-'*int(len(header_format.format(*header)))) #dividing line for the report
+    print('-' * int(len(header_format.format(*header))))  # dividing line for the report
 
-    #Formating and Printing for Donor List
+    # Formating and Printing for Donor List
     donor_format = '{:18}|${:15.2f}|{:16}|${:19.2f}|'
 
-    for step in range(0,no_donors):
-        print(donor_format.format(donor_list[step],
+    for step in range(0, no_donors):
+        print(donor_format.format(donor_data[step][0],
                                   float(donation_total[step]),
                                   int(donation_freq[step]),
                                   float(donation_average[step])))
-    print('\n') # print a line for spacing between the report and menu
-    menu() # after report is generated we will go back to the menu
+    menu()  # after report is generated we will go back to the menu
     pass
+
 
 if __name__ == '__main__':
     menu()
-
-
--- test_mailroom.py
+++ test_mailroom.py
@@ -1,31 +1,81 @@
-
-
-
-
 '''
-I need to add global variables
+A series of unit sets to ensure the logic of the mailroom program
+is working properly using pytest.
+'''
 
- '''
-
-
-
-
-from mailroom import new_donor
+from mailroom import generate_donor_list
 from mailroom import current_donor
+from mailroom import add_new_donor_to_database
+from mailroom import draft_email
+from mailroom import generate_report_data
 
 
-# Global variables to be used to run assert tests
-donor_list = ['Donor1','DonorA']
-donation_history = [[10000], [200, 300]]
 
-
-def test_new_donor():
-    '''Test to verify we are adding a new donor to donor list '''
-    assert new_donor('New Donor', 1200)
-    donor_list == ['Donor1','DonorA','New Donor']
-    donation_history ==  [[10000], [200, 300], [1200]]
+def test_generate_donor_list():
+    '''
+    This test is to ensure that the donor list generated is correct.
+    A serpreate donor list is included here to test seperately.
+    There is no input variable for this function as it relies on values declared globally.
+    '''
+    expected =['Tony Stark', 
+            'Steven Rogers', 
+            'Bruce Wayne', 
+            'Clark Kent', 
+            'Geoffrey Donnell']
+    assert generate_donor_list() == expected
 
 def test_current_donor():
-    assert current_donor('Donor1', 20)
-    donation_history == [[10000, 20], [200, 300]]
-    
+    '''
+    This test is to ensure that the current donor used will have thier
+    donation history appended. 
+    '''
+    expected = [('Tony Stark', [10,1200]),
+                ('Steven Rogers', [10,20]),
+                ('Bruce Wayne',[20,30,40]),
+                ('Clark Kent', [50]),
+                ('Geoffrey Donnell', [60,70])]
+
+    assert current_donor('Tony Stark', 1200) == expected
+
+def test_add_new_donor_to_database():
+    '''
+    This test is to ensure that the new donor and gift is added to the list.
+    '''
+    expected = [('Tony Stark', [10,1200]),
+                ('Steven Rogers', [10,20]),
+                ('Bruce Wayne',[20,30,40]),
+                ('Clark Kent', [50]),
+                ('Geoffrey Donnell', [60,70]),
+                ('Iron Man',[200])]
+    assert add_new_donor_to_database('Iron Man',200) == expected
+
+def test_draft_email():
+    '''
+    This test is to ensure that the draft email contians the correct name and amount
+    '''
+    expected = ('Thank you Batman for your generous donation of $230.12.')
+    assert draft_email('Batman',230.12) == expected
+
+def test_donation_total():
+    '''
+    The generate_report_data() calculates multiple values used. 
+    This test is to check that the correct donation total is calculated
+    '''
+    results = generate_report_data()
+    assert results[1] == [1210, 30, 90, 50, 130, 200]
+
+def test_donation_freq():
+    '''
+    The generate_report_data() calculate multiple values used. 
+    This test is to check that the correct donation freq. is calculated
+    '''
+    results = generate_report_data()
+    assert results[2] == [2, 2, 3, 1, 2, 1]
+
+def test_donation_average():
+    '''
+    The generate_report_data() calculate multiple values used. 
+    This test is to check that the correct donation freq. is calculated
+    '''
+    results = generate_report_data()
+    assert results[3] == [605.00, 15.00, 30.00, 50.00, 65.00, 200.00]
